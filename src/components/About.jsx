865198794283b6a9038d7dff3ee957e14ea66174
ashfakmahmuddev <mdashfakmahmud550@gmail.com>
2026-01-29 15:48:53 +0600

    about part aded

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="py-10">
+      <div className=" md:max-w-280 mx-auto">
+        <div className="md:flex justify-between items-center lg:gap-[178px] md:gap-x-[10px] px-5 lg:px-2">
+          <h1 className="text-black font-poppins font-bold text-[48px] md:text-4xl leading-[72px]">The biggest supplier on the country</h1>
+          <p className="text-black text-base font-poppins font-medium">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
