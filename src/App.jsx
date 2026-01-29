61b84850bfbfecf0d4a11ebfc7a9a4bfec391a8d
gitnayemurrashid <nrsrabon17@gmail.com>
2026-01-29 15:51:01 +0600

    Logo Part

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Logo1 from '../public/Logo1.png'
+import Logo2 from '../public/Logo2.png'
+import Logo3 from '../public/Logo3.png'
+import Logo4 from '../public/Logo4.png'
+
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
+    <div className='max-w-[1200px] mx-auto flex justify-center gap-4 py-10'>
+      <img src={Logo1} alt="logo" />
+      <img src={Logo2} alt="logo" />
+      <img src={Logo3} alt="logo" />
+      <img src={Logo4} alt="logo" />
+    </div>
   )
 }
 
---
+++ src/components/logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo1 from '../../public/Logo1.png'
+
+const logo = () => {
+  return (
+    <div >
+     <img src={Logo1} alt="logo" />
+    </div>
+  )
+}
+
+export default logo
